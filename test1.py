5bcc12d178e0af3c7aa7e18bb1d8b6ebe13c4849
sanjeeva <sanjeevmodugu@gmail.com>
2025-12-18 18:19:41 +0530

    my first file upload

---
+++ test1.py
@@ -0,0 +1,18 @@
+# Print a message
+print("Hello, Python!")
+
+# Store a value in a variable
+name = input("Enter your name: ")
+
+# Use the variable
+print("Nice to meet you,", name)
+
+# Simple condition
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+
